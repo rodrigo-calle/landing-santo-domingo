6f9313c1e3359ef12ee237c519793802ed6da00c
Rodrigo Calle <rodrigoc_0@hotmail.com>
2024-03-10 17:13:35 -0500

    feat: adding main products and seeds

---
+++ bin/products.ts
@@ -0,0 +1,93 @@
+interface Products {
+  name: string;
+  price: number;
+  description: string;
+}
+
+export const products: Products[] = [
+  {
+    name: "Pino Tecunumanii",
+    price: 0.8,
+    description: "Pino Tecunumanii",
+  },
+  {
+    name: "Pino Caribaea",
+    price: 0.7,
+    description: "Pino Caribaea",
+  },
+  {
+    name: "Pino Oocarpa",
+    price: 0.6,
+    description: "Pino Oocarpa",
+  },
+  {
+    name: "Pino Patula",
+    price: 0.5,
+    description: "Pino Patula",
+  },
+  {
+    name: "Pino Taeda",
+    price: 0.4,
+    description: "Pino Taeda",
+  },
+  {
+    name: "Pino Elliotis",
+    price: 0.3,
+    description: "Pino Elliotis",
+  },
+  {
+    name: "Pino Caribaea",
+    price: 0.2,
+    description: "Pino Caribaea",
+  },
+  {
+    name: "Pino Oocarpa",
+    price: 0.1,
+    description: "Pino Oocarpa",
+  },
+  {
+    name: "Pino Patula",
+    price: 0.9,
+    description: "Pino Patula",
+  },
+  {
+    name: "Pino Taeda",
+    price: 0.8,
+    description: "Pino Taeda",
+  },
+  {
+    name: "Pino Elliotis",
+    price: 0.7,
+    description: "Pino Elliotis",
+  },
+  {
+    name: "Pino Caribaea",
+    price: 0.6,
+    description: "Pino Caribaea",
+  },
+  {
+    name: "Pino Oocarpa",
+    price: 0.5,
+    description: "Pino Oocarpa",
+  },
+  {
+    name: "Pino Patula",
+    price: 0.4,
+    description: "Pino Patula",
+  },
+  {
+    name: "Pino Taeda",
+    price: 0.3,
+    description: "Pino Taeda",
+  },
+  {
+    name: "Pino Elliotis",
+    price: 0.2,
+    description: "Pino Elliotis",
+  },
+  {
+    name: "Pino Caribaea",
+    price: 0.1,
+    description: "Pino Caribaea",
+  },
+];
